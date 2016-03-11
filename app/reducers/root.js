b3dd1ec3a0daceba1d25bec9cb7e287aea351df8
Lewis <lewis500@gmail.com>
2016-03-11 15:49:15 -0800

    logit works. kind of unstable.

--- app/reducers/root.js
+++ app/reducers/root.js
@@ -6,7 +6,9 @@ const initialTraders = _(10).range().map(d => {
 		money: 5,
 		price: 1,
 		β: 5, //fraction of spending in reserve
-		sales: [],
+		sales: _.range(2).map(() => {
+			return { time: 0, real_price: 1 }
+		}),
 		/*
 			sale = {time,real_price}
 		*/
@@ -23,44 +25,43 @@ const initialState = {
 	time: 0,
 	β: 5,
 	z: 0,
-	mu: 10
+	mu: 20
 };
 
-
-
 const TF = {
-	buy(buyer, traders, price_index, dt) { //returns a trade
+	choose_seller(buyer_id, intervals) {
+		let draw = Math.random(),
+			seller_id = _.findLastIndex(intervals, d => d < draw);
+		if (buyer_id == seller_id) return this.choose_seller(buyer_id, intervals);
+		else return seller_id;
+	},
+	buy(buyer, intervals, traders, price_index, dt) { //returns a trade
 		const { money, y, β, id } = buyer;
 		const real_balance = money / price_index;
 
-		//DOES DEMAND FOR BALANCES DEPEND ON INCOME OR ONLY CONSUMPTION?
-		// if (_.lt(real_balance, β * y)) return null_trade;
-
-		if (_.gte(real_balance / β * dt, Math.random())) {
-			const seller = _.sample(_.without(traders, buyer));
-			return {
-				buyer_id: id,
-				seller_id: seller.id,
-				price: seller.price
-			};
-		} else {
+		if (_.lt(real_balance / β * dt, Math.random())) {
 			return {
 				buyer_id: id,
 				seller_id: -1,
-				price: -1
 			};
 		}
-
+		// return
+		// let seller_id = _.sample(_.without(traders,buyer)).id;
+			let seller_id = this.choose_seller(id, intervals);
+		return {
+			buyer_id: id,
+			seller_id
+		};
 	}, //end buy
 	calc_y(trader, time, dt) { //returns a trade
 		//count over two seconds
 		const HORIZON = 2;
 		const sales = _.filter(trader.sales, sale => _.gte(sale.time, time - HORIZON)),
 			y = d3.sum(sales, sale => sale.real_price) / HORIZON;
-		let price = trader.price,
-			employment = sales.length / HORIZON; //COULD ALSO USE REAL INCOME?
-		if (employment < .6) price = price * Math.exp(-dt * .02);
-		if (employment > 1.2) price = price * Math.exp(dt * .03);
+		let price = trader.price;
+		let employment = sales.length / HORIZON; //COULD ALSO USE REAL INCOME?
+		if (employment < .7) price = price * Math.exp(-dt * .02);
+		if (employment > 1.3) price = price * Math.exp(dt * .03);
 		return {
 			...trader,
 			sales,
@@ -68,11 +69,7 @@ const TF = {
 			price
 		};
 	}, //end calc_y,
-	calc_denominator(traders, mu) {
-		return d3.sum(traders, d => Math.exp(-d.price * mu));
-	}
 };
-
 
 const reduceTick = (state, action) => {
 	const dt = action.dt / 1000,
@@ -80,27 +77,25 @@ const reduceTick = (state, action) => {
 		mu = state.mu;
 
 	let traders = _.map(state.traders, trader => TF.calc_y(trader, time, dt));
-	const denom = TF.calc_denominator(traders, mu),
+	const numerators = _.map(traders, trader => Math.exp(-trader.price * mu)),
+		denominator = _.sum(numerators),
+		intervals = _.reduce(numerators, (l, num) => {
+			return [...l, l[l.length - 1] + num / denominator];
+		}, [0]),
 		price_index = d3.mean(traders, trader => trader.price);
-		
-	let intervals = _.map(traders, trader => Math.exp(-trader.price * mu));
-
-	intervals = _.reduce(intervals, (l, num) => {
-		l.push(
-			l[l.length - 1] + num / denom
-		);
-		return l;
-	}, [0]);
 
 	const trades = _(traders)
-		.map(buyer => TF.buy(buyer, traders, price_index, dt))
-		.filter(trade => trade.price != -1)
+		.map(buyer => TF.buy(buyer, intervals, traders, price_index, dt))
+		.filter(trade => trade.seller_id !== -1)
 		.value();
 
+	let spending = 0;
+
 	_.forEach(trades, trade => {
-		let { buyer_id, seller_id, price } = trade;
-		let buyer = traders[buyer_id],
-			seller = traders[seller_id];
+		const { buyer_id, seller_id } = trade;
+		const buyer = traders[buyer_id],
+			seller = traders[seller_id],
+			price = seller.price;
 
 		traders[buyer_id] = {
 			...buyer,
@@ -113,18 +108,20 @@ const reduceTick = (state, action) => {
 			money: seller.money + price
 		};
 
+		spending += price;
+
 	});
 
 	const history = _(state.history)
 		.filter(d => (d.time > (time - 10)))
-		.push({ time, spending: d3.sum(trades, d => d.price) })
+		.push({ time, spending })
 		.value();
 
 
 	if (state.z % 50 == 0) {
 		const gdp = d3.sum(history, d => d.spending / price_index) / 10;
 		console.log(gdp);
-		console.log(intervals);
+		// console.log(price_index, Math.log(denominator));
 	}
 
 	return {...state, trades, traders, time, history, z: state.z + 1 };
